935548398b4ee628e93b724df9e758416e75135a
ImagixAI <yasathchannula218@gmail.com>
2026-07-07 22:46:44 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,21 +1,14 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+// Firebase Configuration
+// console.firebase.google.com walin ganna
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB6byqFLhDCOOivmMwpiF0N0cnpjeNwk3k",
-  authDomain: "sl-telecom-d6855.firebaseapp.com",
-  projectId: "sl-telecom-d6855",
-  storageBucket: "sl-telecom-d6855.firebasestorage.app",
-  messagingSenderId: "292283006106",
-  appId: "1:292283006106:web:faed88098e363ecf7bb323",
-  measurementId: "G-E1ZRB05ZNM"
+    apiKey: "AIzaSyB6byqFLhDCOOivmMwpiF0N0cnpjeNwk3k",
+    authDomain: "sl-telecom-d6855.firebaseapp.com",
+    databaseURL: "https://sl-telecom-d6855-default-rtdb.firebaseio.com",
+    projectId: "sl-telecom-d6855",
+    storageBucket: "sl-telecom-d6855.firebasestorage.app",
+    messagingSenderId: "292283006106",
+    appId: "1:292283006106:web:faed88098e363ecf7bb323"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
